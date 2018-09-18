67c7f464bac13562a3896dbcc80b66111516b8eb
zhaochunyu122 <1351059611@qq.com>
2018-09-18 11:36:24 +0800

    denglu

---
+++ APP/js/denglu.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2018/9/16.
+ */
+$(function () {
+
+
+    let btn = $(".btn-success");
+    console.log(btn);
+    btn.click(function () {
+        window.open("shouye.html");
+    })
+})
