4067ea37c4f0ad6ae61c30a693d98c67d79002c5
zhaochunyu122 <1351059611@qq.com>
2018-09-18 18:44:34 +0800

    apple

--- APP/js/denglu.js
+++ APP/js/denglu.js
@@ -2,11 +2,14 @@
  * Created by Administrator on 2018/9/16.
  */
 $(function () {
+    let user=$(".name");
+    let pass=$(".mima");
 
-
-    let btn = $(".btn-success");
-    console.log(btn);
+    let btn = $("button");
     btn.click(function () {
         window.open("shouye.html");
+    })
+    pass.blur(function () {
+        btn.css("backgroundColor","#fe0000");
     })
 })
